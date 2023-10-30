cb89fd2c3d45ac102ae29adc31abdc9134e5dc12
Hanzzz123 <aanzs496@gmail.com>
2023-10-30 17:45:30 +0700

    fixed rrr function and added innerHTML

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 myHTMLNumberInput.onchange = setTwoNumberDecimal;
-
 function setTwoNumberDecimal(event) {
     this.value = parseFloat(this.value).toFixed(2);
+}
+
+function result(){
+    calculateRiskReward();
+
+}
+
+
+
+//create the rrr function
+function calculateRiskReward(){
+
+    let entryPrice = document.getElementById("entryprice").value;
+    let stopLoss = document.getElementById("stoploss").value;
+    let takeProfit = document.getElementById("takeprofit").value;
+
+    let risk = entryPrice - stopLoss;
+    let profit = takeProfit - entryPrice;
+
+    let riskReward = risk / profit;
+
+    document.getElementById("rrr").innerHTML = riskReward;
 }
